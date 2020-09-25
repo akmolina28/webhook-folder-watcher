98aa426d5caa33803a72830f4fbfc5ddec338473
Andrew Molina <2997206+akmolina28@users.noreply.github.com>
2020-09-25 12:35:21 -0500

    Send only the file name, not the full path

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ const axios = require('axios')
 const watch_folder = process.env.WATCH_FOLDER;
 const webhook_url = process.env.WEBHOOK_URL;
 
-const watcher = chokidar.watch(watch_folder, { 
-  ignoreInitial: true 
+const watcher = chokidar.watch('.', { 
+  ignoreInitial: true,
+  cwd: watch_folder
 });
 
 watcher.on('add', (path) => {
